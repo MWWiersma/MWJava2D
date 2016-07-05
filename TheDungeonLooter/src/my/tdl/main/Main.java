e2d154d13211fb1e02eee0ee5d62529d944dda68
dad <MrMichaelanicole@gmail.com>
2016-07-04 21:12:56 -0700

    tutorial4

---
+++ TheDungeonLooter/src/my/tdl/main/Main.java
@@ -0,0 +1,12 @@
+package my.tdl.main;
+
+import my.project.gop.main.GameWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GameWindow frame = new GameWindow("TheDlooter", 1280, 720);
+
+	}
+
+}
